24f93ccf1930c9f02a98f85f5cc6f4e5d897cc05
Giliano G. Novais <vipergiliano@gmail.com>
2025-05-04 15:18:06 -0300

    Implementando BD PostgreSQL localmente.

--- src/main/java/dio/Projeto_API_REST/Application.java
+++ src/main/java/dio/Projeto_API_REST/Application.java
@@ -2,12 +2,13 @@ package dio.Projeto_API_REST;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("dio.Projeto_API_REST.model")
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
+	}	
 }
